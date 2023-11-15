36cc4786be987b17e0ea99b25e48b1e65c1e406c
adrian <estopaceadrian@gmail.com>
2023-11-15 20:26:58 +0800

    use map

--- dictionary/lib/helpers.ex
+++ dictionary/lib/helpers.ex
@@ -10,4 +10,16 @@ defmodule Helpers do
 
   def sum([]), do: 0
   def sum([h | t]), do: h + sum(t)
+
+  # [1, 3, 5] => [ 1, 4, 9]
+  def square([]), do: []
+  def square([h | t]), do: [h * h | square(t)]
+
+  def double([]), do: []
+  def double([h | t]), do: [2 * h | double(t)]
+
+  def map([], _func), do: []
+  # Helpers.map [1,2,3,4], fn x -> 3*x
+  # Helpers.map [1,2,3,4], fn x -> x*x*x
+  def map([h | t], func), do: [func.(h) | map(t, func)]
 end
